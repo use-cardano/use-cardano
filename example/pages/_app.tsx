342f0b7dabf0b1291088e725d4e1d0c114f39de0
Jesper Tullberg <jesper.tullberg@zenta.se>
2022-11-27 15:15:32 +0100

    small formating

--- example/pages/_app.tsx
+++ example/pages/_app.tsx
@@ -3,6 +3,7 @@ import "styles/styles.css"
 import { UseCardanoProvider, WalletProviderToaster } from "use-cardano"
 
 import type { AppProps } from "next/app"
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <UseCardanoProvider>
