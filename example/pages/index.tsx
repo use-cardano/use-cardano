a9c86ac5b4bc52c08d87f6303fe49fa525479f7a
Jesper Tullberg <jesper.tullberg@zenta.se>
2022-12-05 21:37:49 +0100

    avoid global styles

--- example/pages/index.tsx
+++ example/pages/index.tsx
@@ -1,66 +1,86 @@
 import Link from "next/link"
+import styles from "styles/home.module.css"
 
 const Index = () => {
   return (
-    <ul>
-      <li>
-        <Link href="/examples/change-wallet-provider">Change Wallet Provider</Link>
+    <ul className={styles.examplesList}>
+      <li className={styles.item}>
+        <Link className={styles.link} href="/examples/change-wallet-provider">
+          Change Wallet Provider
+        </Link>
       </li>
 
-      <li>
-        <Link href="/examples/change-account">Change Account</Link>
+      <li className={styles.item}>
+        <Link className={styles.link} href="/examples/change-account">
+          Change Account
+        </Link>
       </li>
 
-      <li>
-        <Link href="/examples/change-network">Change Network</Link>
+      <li className={styles.item}>
+        <Link className={styles.link} href="/examples/change-network">
+          Change Network
+        </Link>
       </li>
 
-      <li>
-        <Link href="/examples/transaction">Transaction</Link>
+      <li className={styles.item}>
+        <Link className={styles.link} href="/examples/transaction">
+          Transaction
+        </Link>
       </li>
 
-      <li>
-        <Link href="/examples/configuration/auto-connect-to-nami">
+      <li className={styles.item}>
+        <Link className={styles.link} href="/examples/configuration/auto-connect-to-nami">
           Configuration / Auto Connect to Nami
         </Link>
       </li>
 
-      <li>
-        <Link href="/examples/configuration/auto-connect-to-eternl">
+      <li className={styles.item}>
+        <Link className={styles.link} href="/examples/configuration/auto-connect-to-eternl">
           Configuration / Auto Connect to Eternl
         </Link>
       </li>
 
-      <li>
-        <Link href="/examples/configuration/auto-reconnect">Configuration / Auto Reconnect</Link>
+      <li className={styles.item}>
+        <Link className={styles.link} href="/examples/configuration/auto-reconnect">
+          Configuration / Auto Reconnect
+        </Link>
       </li>
 
-      <li>
-        <Link href="/examples/configuration/auto-connect-and-reconnect">
+      <li className={styles.item}>
+        <Link className={styles.link} href="/examples/configuration/auto-connect-and-reconnect">
           Configuration / Auto Connect and Reconnect
         </Link>
       </li>
 
-      <li>
-        <Link href="/examples/configuration/only-mainnet">Configuration / Only Mainnet</Link>
+      <li className={styles.item}>
+        <Link className={styles.link} href="/examples/configuration/only-mainnet">
+          Configuration / Only Mainnet
+        </Link>
       </li>
 
-      <li>
-        <Link href="/examples/configuration/only-testnet">Configuration / Only Testnet</Link>
+      <li className={styles.item}>
+        <Link className={styles.link} href="/examples/configuration/only-testnet">
+          Configuration / Only Testnet
+        </Link>
       </li>
 
-      <li>
-        <Link href="/examples/configuration/mainnet-and-testnet">
+      <li className={styles.item}>
+        <Link className={styles.link} href="/examples/configuration/mainnet-and-testnet">
           Configuration / Mainnet and Testnet
         </Link>
       </li>
 
-      <li>
-        <Link href="/examples/theming/custom-styles">Theming / Custom Styles</Link>
+      <li className={styles.item}>
+        <Link className={styles.link} href="/examples/theming/custom-styles">
+          Theming / Custom Styles
+        </Link>
       </li>
 
-      <li>
-        <Link href="/examples/customization/custom-wallet-provider-selector">
+      <li className={styles.item}>
+        <Link
+          className={styles.link}
+          href="/examples/customization/custom-wallet-provider-selector"
+        >
           Customization / Custom Wallet Provider Selector
         </Link>
       </li>
