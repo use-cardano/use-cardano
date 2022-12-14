e9904f4bdfbaf7260f957d1ae7bdaa8ad26b35af
Jesper Tullberg <jesper.tullberg@zenta.se>
2022-12-14 12:31:45 +0100

    use port 4200

--- scripts/open.js
+++ scripts/open.js
@@ -1,7 +1,7 @@
 const localtunnel = require("localtunnel")
 const open = require("open")
 
-localtunnel({ port: 3000 }).then((tunnel) => {
+localtunnel({ port: 4200 }).then((tunnel) => {
   console.log("serving examples on", tunnel.url)
 
   open(tunnel.url)
